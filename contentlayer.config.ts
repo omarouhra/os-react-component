78a11f4b6296332f0a17300687cc93090151fdbf
Imad Atyat-Allah <imadatyatalah@gmail.com>
2022-07-17 21:21:30 +0100

    feat: Setup Contentlayer

---
+++ contentlayer.config.ts
@@ -0,0 +1,24 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+
+export const Component = defineDocumentType(() => ({
+  name: "Component",
+  filePathPattern: "components/*.mdx",
+  fields: {
+    title: { type: "string", required: true },
+    description: { type: "string", required: true },
+    date: { type: "date", required: true },
+    tags: { type: "list", required: true, of: { type: "string" } },
+    image: { type: "string", required: true },
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ""),
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: "data",
+  documentTypes: [Component],
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
-const Home = () => {
-  return <section className="bg-red-400">hey</section>;
+import type { GetStaticProps } from "next";
+import type { Component } from "contentlayer/generated";
+import { allComponents } from "contentlayer/generated";
+import { pick } from "contentlayer/client";
+
+type Props = {
+  components: Component[];
+};
+
+const Home = (components: Props) => {
+  return <section className="flex"></section>;
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const components = allComponents.map((component) =>
+    pick(component, ["title", "description", "image", "date", "tags"])
+  );
+
+  return { props: { components } };
 };
 
 export default Home;
