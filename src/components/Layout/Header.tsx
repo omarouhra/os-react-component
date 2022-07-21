87cdfb2c55d2a1ad9de155b6b82c0bde3ce5bc13
Imad Atyat-Allah <imadatyatalah@gmail.com>
2022-07-21 13:48:41 +0100

    feat: added Moon and Sun icons

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import OsIcon from "@/components/icons/OsIcon";
 import Link from "next/link";
 import { useTheme } from "next-themes";
+import MoonIcon from "../icons/MoonIcon";
+import SunIcon from "../icons/SunIcon";
 
 function Header() {
   const [mounted, setMounted] = useState(false);
@@ -27,37 +29,7 @@ function Header() {
           }}
           type='button'
         >
-          {theme === "light" ? (
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-6 w-6'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-6 w-6'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z'
-              />
-            </svg>
-          )}
+          {theme === "light" ? <MoonIcon /> : <SunIcon />}
         </button>
       </nav>
     </div>
