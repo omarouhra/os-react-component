e30978e5168c060235e776a57f7ade115d7dabfd
omarouhra <omarmaroc26@gmail.com>
2022-07-21 14:47:26 +0100

    feat: animation on theme toggle ⭐

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,7 +4,7 @@ import Footer from "@/components/Layout/Footer";
 
 function index({ children }: any) {
   return (
-    <section className='bg-gray-50 dark:bg-gray-900'>
+    <section className='transitions bg-gray-50 dark:bg-gray-900'>
       <div className='mx-auto max-w-4xl px-5 lg:px-0 '>
         <Header />
         {children}
--- src/components/TechnologyCard.tsx
+++ src/components/TechnologyCard.tsx
@@ -9,8 +9,9 @@ type cardType = {
 function TechnologyCard({ technology, content, gradient }: cardType) {
   return (
     <div
-      className={`transitions rounded-lg bg-gradient-to-r hover:shadow-xl sm:h-[120px] md:h-auto md:max-w-[230px] lg:hover:scale-105 ${gradient} p-1.5`}>
-      <div className='h-full rounded-md bg-white py-4 px-6 dark:bg-gray-900 flex items-center justify-center'>
+      className={`transitions rounded-lg bg-gradient-to-r hover:shadow-xl sm:h-[120px] md:h-auto md:max-w-[230px] lg:hover:scale-105 ${gradient} p-1.5`}
+    >
+      <div className='transitions flex h-full items-center justify-center rounded-md bg-white py-4 px-6 dark:bg-gray-900 md:items-start'>
         <p className='font-light text-gray-600 dark:text-gray-300'>
           {" "}
           <span className='font-bold text-black dark:text-white '>
