2571fe04b56b62f8d807d0fb08a44a40a01f7da1
omarouhra <omarmaroc26@gmail.com>
2022-07-21 13:44:39 +0100

    fix: technology card height fix 2️⃣

--- src/components/TechnologyCard.tsx
+++ src/components/TechnologyCard.tsx
@@ -9,7 +9,7 @@ type cardType = {
 function TechnologyCard({ technology, content, gradient }: cardType) {
   return (
     <div
-      className={`transitions rounded-lg bg-gradient-to-r hover:shadow-xl h-[120px] md:h-auto md:max-w-[230px] lg:hover:scale-105 ${gradient} p-1.5`}>
+      className={`transitions rounded-lg bg-gradient-to-r hover:shadow-xl sm:h-[120px] md:h-auto md:max-w-[230px] lg:hover:scale-105 ${gradient} p-1.5`}>
       <div className='h-full rounded-md bg-white py-4 px-6 dark:bg-gray-900 flex items-center justify-center'>
         <p className='font-light text-gray-600 dark:text-gray-300'>
           {" "}
