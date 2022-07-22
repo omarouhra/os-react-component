504796e2ec40a9ec204cd31b34d7c713d45d7007
omarouhra <omarmaroc26@gmail.com>
2022-07-22 08:35:36 +0100

    feat: implement Component page 🚀

---
+++ src/components/HeroTitle.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HeroTitle() {
+  return (
+    <h1 className='mb-6 max-w-3xl text-3xl  font-bold dark:text-white lg:text-5xl lg:leading-tight'>
+      Ready to use React{" "}
+      <span className='bg-gradient-to-r from-blue-700 to-cyan-300 bg-clip-text text-3xl font-extrabold text-transparent lg:text-5xl'>
+        Components
+      </span>{" "}
+      with high end code quality 🚀
+    </h1>
+  );
+}
+
+export default HeroTitle;
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,8 +4,8 @@ import Footer from "@/components/Layout/Footer";
 
 function index({ children }: any) {
   return (
-    <section className='transitions bg-gray-50 dark:bg-gray-900'>
-      <div className='mx-auto max-w-4xl px-5 lg:px-0 '>
+    <section className='transitions  min-h-screen bg-gray-50/95 dark:bg-gray-900'>
+      <div className='mx-auto max-w-4xl px-5 lg:px-0'>
         <Header />
         {children}
         <Footer />
