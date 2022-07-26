37bac765d7b58ff200267bbc50d25980edb80784
omarouhra <omarmaroc26@gmail.com>
2022-07-26 14:21:19 +0100

    feat: preview/code layout

--- src/components/ComponentPreview.tsx
+++ src/components/ComponentPreview.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import CodeIcon from "./icons/CodeIcon";
+import CopyIcon from "./icons/CopyIcon";
+import PreviewIcon from "./icons/PreviewIcon";
 
 function ComponentPreview() {
-  return <div className='h-[500px]  bg-gray-700 rounded-md'></div>;
+  const [activeTab, setActivePanel] = useState("preview");
+  return (
+    <section className=''>
+      <div className='flex items-center justify-between py-4'>
+        <p className='text-xl font-semibold text-gray-500 dark:text-white'>
+          Simple Minimal Form
+        </p>
+        <div className='flex items-center justify-center space-x-6'>
+          <div className='flex '>
+            <button
+              className={`short-transitions rounded-l-lg py-3 px-6 ${
+                activeTab === "preview" && " bg-gray-200 dark:bg-gray-600"
+              }`}
+              onClick={() => setActivePanel("preview")}
+            >
+              <PreviewIcon />
+            </button>
+            <button
+              className={`rounded-r-lg py-3 px-6  short-transitions${
+                activeTab === "code" && " bg-gray-200 dark:bg-gray-600"
+              }`}
+              onClick={() => setActivePanel("code")}
+            >
+              <CodeIcon />
+            </button>
+          </div>
+          <button className='short-transitions rounded-lg py-3 px-3 hover:bg-blue-500 hover:text-white dark:hover:bg-gray-600'>
+            <CopyIcon />
+          </button>
+        </div>
+      </div>
+
+      {activeTab === "preview" ? (
+        <div className='flex h-[500px]  items-center justify-center rounded-md bg-gradient-to-r from-blue-700 to-cyan-300 '>
+          {" "}
+          Preview
+        </div>
+      ) : (
+        <div className='flex  h-[500px] items-center justify-center rounded-md bg-gray-700'>
+          Code
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default ComponentPreview;
---
+++ src/components/icons/PreviewIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function PreviewIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className='h-6 w-6'
+      fill='none'
+      viewBox='0 0 24 24'
+      stroke='currentColor'
+      strokeWidth='2'
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M8 13v-1m4 1v-3m4 3V8M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z'
+      />
+    </svg>
+  );
+}
+
+export default PreviewIcon;
