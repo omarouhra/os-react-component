d11bac836a54609e7bb113686dc38c5ceca12212
Imad Atyat-Allah <imadatyatalah@gmail.com>
2022-07-27 13:48:05 +0100

    fix: fix language select

--- src/components/languageSelect.tsx
+++ src/components/languageSelect.tsx
@@ -14,16 +14,24 @@ const LanguageSelect = () => {
         setCurrentLanguage("typescript");
   }, [currentLanguage]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) =>
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    e.preventDefault();
+
+    // @ts-ignore
+    setCurrentLanguage(e.target.value);
+
     localStorage.setItem("language", e.target.value);
+  };
 
   return (
     <select
       className='short-transitions rounded-r-lg py-3 px-6'
       id='languageSelect'
-      defaultValue={currentLanguage}
+      // defaultValue={currentLanguage}
+      value={currentLanguage}
       onChange={handleChange}
     >
+      <option value=''></option>
       <option value='javascript'>JavaScript</option>
       <option value='typescript'>TypeScript</option>
     </select>
