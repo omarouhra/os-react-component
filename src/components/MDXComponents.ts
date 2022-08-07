33e31c71803fd1787b38db2e62eff34c84027a87
Imad Atyat-Allah <imadatyatalah@gmail.com>
2022-08-07 14:10:03 +0100

    feat: added callout component

--- src/components/MDXComponents.ts
+++ src/components/MDXComponents.ts
@@ -1,9 +1,13 @@
 import ComponentPreview from "./ComponentPreview";
+
+// Components
 import UIHeader from "@/ui/Header/Header";
+import UICallout from "@/ui/Callout/Callout";
 
 const MDXComponents = {
   ComponentPreview,
   UIHeader,
+  UICallout,
 };
 
 export default MDXComponents;
