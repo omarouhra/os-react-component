58af1ca9833dd7b06e978f50f72b1d804046eb13
Imad Atyat-Allah <imadatyatalah@gmail.com>
2022-08-10 14:51:43 +0100

    feat: added hero component

--- src/components/MDXComponents.ts
+++ src/components/MDXComponents.ts
@@ -3,11 +3,13 @@ import ComponentPreview from "./ComponentPreview";
 // Components
 import UIHeader from "@/ui/Header/Header";
 import UICallout from "@/ui/Callout/Callout";
+import UIHero from "@/ui/Heros/Hero";
 
 const MDXComponents = {
   ComponentPreview,
   UIHeader,
   UICallout,
+  UIHero,
 };
 
 export default MDXComponents;
