493c6bcf12f01b73c58b60b2ec8c3b5245f69967
omarouhra <omarmaroc26@gmail.com>
2022-08-15 19:26:25 +0100

    feat: splash screens

--- src/pages/heros.tsx
+++ src/pages/heros.tsx
@@ -1,76 +1,98 @@
 import React from "react";
-import Image from "next/image";
-import Link from "next/link";
+import { motion } from 'framer-motion'
+
+
+
+
+//Container height animation
+const container = {
+  hidden: {
+    height: "100%",
+  },
+  visible: {
+    height: "0%",
+    transition: { ease: [0.455, 0.03, 0.515, 0.955], duration: 1.5 }
+  }
+}
+
+
+// Container children delay
+const delayChildren = {
+  animate: {
+    transition: {
+      staggerChildren: 1.4
+    }
+  }
+
+}
+//Fade In animation
+const fadeIn = {
+  hidden: {
+    opacity: 0,
+    y: 100,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { ease: [0.455, 0.03, 0.515, 0.955], duration: 2 }
+  }
+}
 
 function heros() {
   return (
-    <>
-      <section className='flex flex-col space-y-12 py-12 md:flex-row-reverse md:items-center md:justify-between md:space-y-0 '>
-        <div className='relative h-52 w-52 rounded-full transition duration-300 hover:shadow-xl md:h-60 md:w-60'>
-          <Image
-            src='https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80'
-            layout='fill'
-            alt='person'
-            objectFit='cover'
-            className='rounded-full'
-          />
-        </div>
-        <div className='md:w-2/3 '>
-          <span className='mb-2 text-xl font-light md:text-3xl '>
-            Hello 👋!
-          </span>
-          <h1 className='mb-2 text-4xl font-semibold md:mb-5 md:text-6xl md:font-bold'>
-            My name is{" "}
-            <span className='bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent'>
-              Fatima
-            </span>
-          </h1>
-          <p className='mb-6 text-base font-normal text-gray-400 dark:text-gray-100'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit
-            ducimus eveniet, deleniti adipisci corrupti deserunt voluptatibus.
-          </p>
+    <div className="space-y-12">
 
-          <Link href='/'>
-            <a className='flex items-center space-x-1 hover:underline'>
-              Visite the website &rarr;
-            </a>
-          </Link>
-        </div>
-      </section>
+      {/* Splash Screen 1 */ }
+      <motion.section
+        initial="hidden"
+        animate="visible"
+        className="relative py-8 bg-gray-50 dark:bg-gray-800 h-[500px] flex items-center justify-center shadow-2xl">
+        <motion.div
+          variants={ container }
+          className="z-50 absolute top-0 w-full h-full bg-blue-900"></motion.div>
+        <motion.h1
+          variants={ fadeIn }
+          className='mb-6 max-w-3xl text-xl font-bold text-center dark:text-white lg:text-3xl lg:leading-tight'>
+          Ready to use React{ " " }
+          <span className='bg-gradient-to-r from-blue-700 to-cyan-300 bg-clip-text text-xl font-extrabold text-transparent lg:text-3xl'>
+            Components
+          </span>{ " " }
+          with high end code quality 🚀
+        </motion.h1>
+      </motion.section>
 
-      <section className='flex w-full flex-col items-center justify-center space-y-12 py-12  '>
-        <div className='relative h-52 w-52 rounded-full transition duration-300 hover:shadow-xl md:h-60 md:w-60'>
-          <Image
-            src='https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80'
-            layout='fill'
-            alt='person'
-            objectFit='cover'
-            className='rounded-full'
-          />
-        </div>
-        <div className='flex flex-col items-center text-center'>
-          <span className='mb-2 text-xl font-light md:text-3xl '>
-            Hello 👋!
-          </span>
-          <h1 className='mb-2 text-4xl font-semibold md:mb-5 md:text-6xl md:font-bold'>
-            My name is{" "}
-            <span className='bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent'>
-              Fatima
-            </span>
-          </h1>
-          <p className='mb-6 text-base font-normal text-gray-400 dark:text-gray-100'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit
-            ducimus eveniet, deleniti adipisci corrupti deserunt voluptatibus.
-          </p>
 
-          <Link href='/'>
-            <a className='flex items-center space-x-1 hover:underline'>
-              Visite the website &rarr;
-            </a>
-          </Link>
-        </div>
-      </section>
-    </>
+      {/* Splash Screen 2 */ }
+      <motion.section
+        initial="hidden"
+        animate="visible"
+        className="relative py-8 bg-gray-50 dark:bg-gray-800 h-[500px] flex items-center justify-center shadow-2xl">
+
+
+        <motion.div
+          variants={ delayChildren }
+          className="z-50 absolute top-0 w-full h-full flex">
+          <motion.div
+            variants={ container }
+            className="w-1/3 h-full bg-blue-900"></motion.div>
+          <motion.div
+            variants={ container }
+            className="w-1/3 h-full bg-blue-500"></motion.div>
+          <motion.div
+            variants={ container }
+            className="w-1/3 h-full bg-blue-300"></motion.div>
+        </motion.div>
+        <motion.h1
+          variants={ fadeIn }
+          className='mb-6 max-w-3xl text-xl font-bold text-center dark:text-white lg:text-3xl lg:leading-tight'>
+          Ready to use React{ " " }
+          <span className='bg-gradient-to-r from-blue-700 to-cyan-300 bg-clip-text text-xl font-extrabold text-transparent lg:text-3xl'>
+            Components
+          </span>{ " " }
+          with high end code quality 🚀
+        </motion.h1>
+      </motion.section>
+    </div >
   );
 }
 
