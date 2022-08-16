6a8367d4104fad1361ddaf61ada2a24f3258d971
omarouhra <omarmaroc26@gmail.com>
2022-08-16 15:56:17 +0100

    feat: text animations

--- src/pages/texts.tsx
+++ src/pages/texts.tsx
@@ -1,54 +1,116 @@
 import Link from "next/link";
 import React from "react";
+import { motion } from 'framer-motion'
 
 function texts() {
+
+
+    const text = 'Letter Animation Text';
+
+    // splite words
+    const splitWords = text.split(" ");
+
+    // splite words to letters
+    const words = splitWords.map(word => {
+        return word.split('')
+    })
+
+    //  adding spacing
+    words.map(word => {
+        return word.push("\u00A0")
+    })
+
+
+
+    // letter animation
+    const letterAnimation = {
+        hidden: {
+            y: "40%",
+            opacity: 0,
+            color: "#0055FF",
+
+            transition: { ease: [0.455, 0.03, 0.515, 0.955], duration: 0.85 }
+        },
+        visible: {
+            y: 0,
+            opacity: 1,
+            color: "#FF0088",
+
+            transition: { ease: [0.455, 0.03, 0.515, 0.955], duration: 0.75 }
+        }
+    };
+    // Text color animation
+    const colorAnimation = {
+        hidden: {
+            color: "#FF0088",
+        },
+        visible: {
+            color: "#0055FF",
+
+            transition: { ease: [0.455, 0.03, 0.515, 0.955], duration: 0.75 }
+
+        }
+    };
+
+    // letter container animation
+    const containerAnimation = {
+        visible: {
+            transition: {
+                staggerChildren: 0.08,
+            }
+        }
+    }
+
+
+
+
     return (
         <section className="space-y-12">
-            <div className="py-12 bg-white dark:bg-black/10 px-5 rounded-md shadow">
-                <h2 className='mb-3 text-xl text-blue-700 dark:text-cyan-500 md:text-2xl'>
-                    Welcome To Os React Components 🔥
-                </h2>
-                <p className='mb-4 text-base font-normal leading-6 text-gray-400 dark:text-gray-300'>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat quia ratione quod ipsa consectetur totam, similique eius doloremque, ut harum ullam cupiditate non labore est autem laboriosam deleniti cumque illum incidunt consequuntur, omnis commodi deserunt. Laborum quae magnam quis possimus!
-                </p>
-                <Link href='/' >
-                    < a className='flex items-center space-x-1 hover:underline'>
-                        Visite the website &rarr;
-                    </ a>
-                </Link >
-            </div>
-            <div className="py-12 bg-white dark:bg-black/10 px-5 rounded-md shadow text-center">
-                <h2 className='mb-3 text-xl text-blue-700 dark:text-cyan-500 md:text-2xl'>
-                    Welcome To Os React Components 🔥
-                </h2>
-                <p className='mb-4 text-base font-normal leading-6 text-gray-400 dark:text-gray-300'>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat quia ratione quod ipsa consectetur totam, similique eius doloremque, ut harum ullam cupiditate non labore est autem laboriosam deleniti cumque illum incidunt consequuntur, omnis commodi deserunt. Laborum quae magnam quis possimus!
-                </p>
-                <Link href='/' >
-                    < a className='space-x-1 hover:underline'>
-                        Visite the website &rarr;
-                    </ a>
-                </Link >
-            </div>
-            <div className="py-12 bg-white dark:bg-black/10 px-5 rounded-md shadow flex flex-col space-y-8 md:flex-row-reverse md:items-center md:justify-center md:space-y-0  ">
-                <div className=" w-full md:w-1/2 h-[300px] bg-gradient-to-r from-blue-500 to-cyan-500 rounded-md shadow-lg">
-                    {/* Here goes the image */ }
-                </div>
-                <div className="w-full md:w-1/2">
 
-                    <h2 className='mb-3 text-xl text-blue-700 dark:text-cyan-500 md:text-2xl'>
-                        Welcome To Os React Components 🔥
-                    </h2>
-                    <p className='mb-4 text-base font-normal leading-6 text-gray-400 dark:text-gray-300'>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat quia ratione quod ipsa consectetur totam, similique eius doloremque, ut harum
-                    </p>
-                    <Link href='/' >
-                        < a className='space-x-1 hover:underline'>
-                            Visite the website &rarr;
-                        </ a>
-                    </Link >
-                </div>
-            </div>
+            {/* Letter Animation  */ }
+            <motion.div
+                initial='hidden'
+                animate='visible'
+                className="py-12 bg-white dark:bg-black/10 px-5 rounded-md shadow-2xl border text-center ">
+
+                <motion.div
+
+                    variants={ containerAnimation }
+                >
+
+                    { words.flat().map((element, index) => {
+                        return (
+                            <motion.span
+                                className='inline-block text-3xl md:text-7xl font-cal mt-3  transition duration-500 '
+                                variants={ letterAnimation }
+                                key={ index }>{ element }</motion.span>
+                        )
+                    }) }
+                </motion.div>
+            </motion.div>
+
+
+            {/* gradient Text Animation  */ }
+            <motion.div
+                initial='hidden'
+                animate='visible'
+                className="py-12 bg-white dark:bg-black/10 px-5 rounded-md shadow-2xl border text-center ">
+
+                <motion.div
+
+                    variants={ containerAnimation }
+                >
+
+                    { words.flat().map((element, index) => {
+                        return (
+                            <motion.span
+                                className='inline-block text-3xl md:text-7xl font-cal mt-3  transition duration-500 '
+                                variants={ colorAnimation }
+                                key={ index }>{ element }</motion.span>
+                        )
+                    }) }
+                </motion.div>
+            </motion.div>
         </section >
     );
 }
