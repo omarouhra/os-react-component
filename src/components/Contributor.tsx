3c3a9608ed2349e91b96885748b3e0fcd8126f30
omarouhra <omarmaroc26@gmail.com>
2022-08-29 11:58:51 +0100

    feat: contributors images blur

--- src/components/Contributor.tsx
+++ src/components/Contributor.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 
 type ContributorType = {
@@ -7,22 +7,30 @@ type ContributorType = {
   src: string;
 };
 function Contributor({ name, role, src }: ContributorType) {
+  const [isLoading, setLoading] = useState(true);
+
   return (
     <div className=' flex w-[full] items-center space-x-4 rounded-md py-4 md:px-4 '>
-      <div className='transitions relative h-24 w-24 rounded-full shadow-lg'>
+      <div className='transitions relative h-24 w-24 rounded-full shadow-lg overflow-hidden'>
         <Image
-          src={src}
+          src={ src }
           layout='fill'
-          alt={name}
+          alt={ name }
           objectFit='cover'
-          className='rounded-full'
+          className={ ` duration-500 ease-in-out rounded-full
+            ${isLoading
+              ? 'scale-110 blur-2xl grayscale'
+              : 'scale-100 blur-0 grayscale-0'
+            }
+            `}
+          onLoadingComplete={ () => setLoading(false) }
         />
       </div>
       <div className='flex flex-col space-y-2'>
         <p className='text-lg font-semibold text-gray-800 dark:text-white md:text-xl'>
-          {name}
+          { name }
         </p>
-        <span className='text-sm font-light text-gray-400'>{role}</span>
+        <span className='text-sm font-light text-gray-400'>{ role }</span>
       </div>
     </div>
   );
