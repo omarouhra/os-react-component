5aaaf3d03127c73216eecd9644ff229c4ceb0b81
Omar Ouhra <omarmaroc26@gmail.com>
2022-09-05 22:09:31 +0100

    feat: add splitbee analitics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,6 +22,7 @@ const Document = () => {
           href='/favicon-16x16.png'
         />
         <link rel='manifest' href='/site.webmanifest' />
+        <script async src='https://cdn.splitbee.io/sb.js'></script>
       </Head>
       <body>
         <Main />
