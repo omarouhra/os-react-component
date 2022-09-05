bacb2265f334d4fbb95f169ff4ab1cb9be040c63
omarouhra <omarmaroc26@gmail.com>
2022-09-05 14:36:54 +0100

    feat: og & favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,34 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Document = () => {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta property="og:title" content="os components 🚀" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://os-c.vercel.app/" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:image" content="/og.png" />
+        <meta property='og:image:width' content='1200' />
+        <meta property='og:image:height' content='630' />
+      </Head>
       <body>
         <Main />
         <NextScript />
