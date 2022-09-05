3835d81dd5ef83a9fc1db629fef867923c30d72d
omarouhra <omarmaroc26@gmail.com>
2022-09-05 14:28:11 +0100

    feat: title annimation

--- src/components/HeroTitle.tsx
+++ src/components/HeroTitle.tsx
@@ -5,17 +5,9 @@ function HeroTitle() {
   return (
     <h1 className='mb-6 max-w-3xl font-cal text-4xl dark:text-white lg:text-5xl lg:leading-tight'>
       Animated React{" "}
-      <motion.p
-        initial={{ rotate: 0, y: 0 }}
-        animate={{
-          rotate: [0, -30, 0],
-          y: [0, -40, 0],
-          transition: { duration: 2.5, delay: 1 },
-        }}
-        className='inline-block bg-gradient-to-r from-blue-700 to-cyan-300 bg-clip-text text-4xl font-extrabold text-transparent lg:text-5xl'
-      >
+      <span className='bg-gradient-to-r from-blue-700 to-cyan-300 bg-clip-text text-4xl font-extrabold text-transparent lg:text-5xl'>
         Components
-      </motion.p>{" "}
+      </span>{" "}
       with high end code quality
       <div className='inline-block animate-rocket'>
         <motion.p
