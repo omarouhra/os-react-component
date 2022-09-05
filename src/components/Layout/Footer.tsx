38f3a393023be8667e4245516954519a427d37b6
Omar Ouhra <hi@omarcode.me>
2022-09-05 21:35:21 +0100

    Update Footer.tsx

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className='w-full py-20 text-center font-bold dark:text-gray-200'>
-      Created using Next.js in 🇲🇦
+    <footer className='w-full py-20 text-center font-cal dark:text-gray-200'>
+      Made in Morocco 🇲🇦
     </footer>
   );
 }
